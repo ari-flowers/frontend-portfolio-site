bcb458cbb62ffc4a849df058e19bca86c988b985
Ari Flowers <87172647+ari-flowers@users.noreply.github.com>
2021-10-24 19:51:35 -0700

    Inital Commit

---
+++ src/data.js
@@ -0,0 +1,70 @@
+export const projects = [
+  {
+    title: "Pokegotchi",
+    subtitle: "HTML, CSS, JavaScript",
+    description:
+      "A twist on the classic Tamagotchi handheld game created with JavaScript classes and CSS animations. Focus on interactivity, clean JavaScript logic, and nostalgic animation style using CSS. New features and refactoring currently underway.",
+    image: "./pokegatchi-gif.gif",
+    link: "https://github.com/ari-flowers/pokegatchi",
+  },
+  {
+    title: "Bookmarks Bar",
+    subtitle: "HTML, CSS, JavaScript, Express, MongoDB, Node, Heroku",
+    description:
+      "A collaborative group project for a software engineering student centered resource site. Developed the front end layout and page designs using responsive libraries such as Bootstrap. Implemented full CRUD and strengthened knowledge about restful routing.",
+    image: "./bookmarksbar.png",
+    link: "https://github.com/jmalabed/project1",
+  },
+  {
+    title: "DevNet",
+    subtitle: "React, Express, MongoDB, Node, Heroku",
+    description:
+      "DevNet provides a space for current students, alumni, and new developers to post interview questions they've encountered during their job search. The purpose of this is to provide insight for those yet to start their job search, and provide live data for those already searching. The goal is to build community, offer support and solutions that may not have been previously considered.",
+    image: "./devnet.png",
+    link: "https://github.com/adaruiz93/devnet-backend",
+  },
+  {
+    title: "Chromatify",
+    subtitle: "React, JavaScript, Express, MongoDB, Node, OAuth",
+    description:
+      "A music visualization tool utilizing the Spotify API to make calls and display song and artist data. This was a solo development project focused on React functionality and interacting with APIs with OAuth. Future work includes implementing React Drag and Drop and D3 data visualization of user created playlists.",
+    image: "./chromatifyhome.png",
+    link: "https://chromatify.surge.sh/",
+  },
+];
+
+// export const testimonials = [
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/1.jpg",
+//     name: "Jesse Hicks",
+//     company: "Zoozle",
+//   },
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/94.jpg",
+//     name: "Ruben Alvarez",
+//     company: "Dooble.io",
+//   },
+// ];
+
+export const skills = [
+  "HTML",
+  "CSS (Bootstrap, Tailwind, Grid, Flex)",
+  "React",
+  "JavaScript",
+  "Python",
+  "Express",
+  "Django",
+  "Node",
+  "MongoDB",
+  "PostgreSQL",
+  "SQL",
+  "Mongoose",
+  "Git",
+  "GitHub",
+  "AWS",
+  "Heroku"
+];
